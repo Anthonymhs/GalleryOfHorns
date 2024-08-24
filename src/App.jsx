843d70e859d5cbbcebe1d37c33614f8068005b71
ANTHONY <anthonymhs0320@gmail.com>
2024-08-24 08:13:19 -0500

    mostrar un modal para cada img

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import './App.css'
-import SelectBeast from './SelectBeast'
+
 import data from './data.json';
 import Header from './Header';
 import Gallery from './Gallery';
+import SelectBeast from './SelectBeast';
 import Footer from './Footer';
-import { useState } from 'react';
 
 function App() {
-  
   const [displayModal, setDisplayModal] = useState (false);
   const [selectedBeast, setSelectedBeast] = useState ({});
-
   function mostrarModal(name) {
-    const beastWithName = data.find(beast => beast.title === name ) 
-
-    setSelectedBeast (beastWithName);
+    const beastWithName = data.find(beast => beast.title===name);
+    setSelectedBeast(beastWithName);
     setDisplayModal(true);
-    
   }
   function handleClose(){
     setDisplayModal(false);
   }
-  
-  console.log (selectedBeast);
-
-    return (
-      <>
-        <Header/>
-        <Gallery mostrarModal={mostrarModal} allbeast={data}/>
-        <SelectBeast selectedBeast={selectedBeast} show = {displayModal} handleClose = {handleClose}/>
-        <Footer/>
-      </>
-    );
-  }
-
+  return (
+    <>
+    <div>
+      <Header/>
+      <Gallery mostrarModal={mostrarModal} allbeast={data}/>
+      <SelectBeast selectedBeast={selectedBeast} show={displayModal} handleClose={handleClose} />
+      <Footer/>
+    </div>
+    </>
+  );
+}
 export default App;
